09a4d19a0bb0f5f68d99e0463b690d3f62029d49
Ovie Revaldi <ovierevaldi@gmail.com>
2024-10-30 10:33:53 +0700

    add pdf extract library

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import express from 'express';
 const app = express()
 const port = process.env.PORT | 3000;
 
+app.use('/node_modules', express.static('node_modules'));
+
 app.set('view engine', 'ejs');
 
 app.set('views', './views')
